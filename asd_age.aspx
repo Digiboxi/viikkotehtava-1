bf6eaca88de6f25051f9ab011790a429cec66546
Ville <g2007@student.jamk.fi>
2013-09-19 10:02:27 +0300

    Viikon 38 tehtävä
    Life... Finds a way.

---
+++ asd_age.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="asd_age.aspx.cs" Inherits="asd_age" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:Label ID="paivamaara" runat="server" Text="Label" OnInit="paivamaara_Init"></asp:Label><br />
+    <asp:Label ID="valittuPaiva" runat="server" Text="Label" OnInit="valittuPaiva_Init"></asp:Label><br />
+    <asp:Label ID="paivienErotus" runat="server" Text="Label" OnInit="paivienErotus_Init"></asp:Label><br />
+    <asp:Button ID="vuosiTaakse" runat="server" Text="<-- Vuosi" OnClick="vuosiTaakse_Click" />
+    <asp:Button ID="vuosiEteen" runat="server" Text="Vuosi -->" OnClick="vuosiEteen_Click" />
+    <br />
+    <asp:Calendar ID="kalenteri" runat="server" Height="210px" Width="231px" OnInit="kalenteri_Init" OnSelectionChanged="valittuPaiva_Init"></asp:Calendar>
+</asp:Content>
+
---
+++ asd_age.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class asd_age : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+
+    protected void paivamaara_Init(object sender, EventArgs e)
+    {
+        String currentDate = DateTime.Now.GetDateTimeFormats('d')[0];
+        this.paivamaara.Text = "Tänään on: " + currentDate;
+    }
+    protected void valittuPaiva_Init(object sender, EventArgs e)
+    {
+        String valittuPaiva = Convert.ToString(this.kalenteri.SelectedDate.Day);
+        String valittuKuukausi = Convert.ToString(this.kalenteri.SelectedDate.Month);
+        String valittuVuosi = Convert.ToString(this.kalenteri.SelectedDate.Year);
+        this.valittuPaiva.Text = "Valittu päivä: " + valittuPaiva + "." + valittuKuukausi + "." + valittuVuosi;
+        paivienErotusF();
+    }
+    protected void paivienErotus_Init(object sender, EventArgs e)
+    {
+        this.paivienErotus.Text = "Päivien erotus: 0";
+    }
+    protected void paivienErotusF()
+    {
+        String erotus = Convert.ToString(Math.Abs((this.kalenteri.SelectedDate - DateTime.Today).Days));
+        this.paivienErotus.Text = "Päivien erotus: " + erotus;
+    }
+    protected void kalenteri_Init(object sender, EventArgs e)
+    {
+        this.kalenteri.SelectedDate = DateTime.Now;
+        this.kalenteri.VisibleDate = DateTime.Now;
+        String valittuPaiva = DateTime.Now.GetDateTimeFormats('d')[0];
+        this.valittuPaiva.Text = "Valittu päivä: " + valittuPaiva;
+    }
+    protected void vuosiTaakse_Click(object sender, EventArgs e)
+    {
+        this.kalenteri.VisibleDate = this.kalenteri.VisibleDate.AddYears(-1);
+    }
+    protected void vuosiEteen_Click(object sender, EventArgs e)
+    {
+        this.kalenteri.VisibleDate = this.kalenteri.VisibleDate.AddYears(1);
+    }
+}
