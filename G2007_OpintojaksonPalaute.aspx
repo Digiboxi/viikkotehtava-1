452a1ff56caede7c03f74106c3f4c526ab4932bd
Ville <g2007@student.jamk.fi>
2013-10-10 12:17:15 +0300

    Tuntitehtäviä
    Palautteen anto jne

---
+++ G2007_OpintojaksonPalaute.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="G2007_OpintojaksonPalaute.aspx.cs" Inherits="G2007_OpintojaksonPalaute" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <table border="1">
+        <tr>
+            <td>
+                <h1>Anna palaute</h1>
+                Nimi:<asp:TextBox ID="txtNimi" runat="server"></asp:TextBox><br />
+                Olen oppinut:<asp:TextBox ID="txtOppinut" runat="server" TextMode="MultiLine"></asp:TextBox><br />
+                Haluan oppia:<asp:TextBox ID="txtHaluanoppia" runat="server" TextMode="MultiLine"></asp:TextBox>
+            </td>
+            <td>
+                <h1>palaute jatkuu</h1>
+                Hyvää:<asp:TextBox ID="txtHyvaa" runat="server" TextMode="MultiLine"></asp:TextBox><br />
+                Parannettavaa:<asp:TextBox ID="txtParannettavaa" runat="server" TextMode="MultiLine"></asp:TextBox><br />
+                Muuta:<asp:TextBox ID="txtMuuta" runat="server" TextMode="MultiLine"></asp:TextBox><br />
+                <asp:Button ID="submit" runat="server" Text="Lähetä palaute" OnClick="NapinPainallus" />
+            </td>
+        </tr>
+        <tr>
+            <td><asp:Button ID="nayta" runat="server" Text="Näytä XML" OnClick="nayta_Click" /></td>
+        </tr>
+    </table>
+    <div id="teksti" runat="server">
+    </div>
+</asp:Content>
+
---
+++ G2007_OpintojaksonPalaute.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+public partial class G2007_OpintojaksonPalaute : System.Web.UI.Page
+{
+    protected void NapinPainallus(object sender, EventArgs e)
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.Load(MapPath("~/App_Data/Palautteet.xml"));
+
+        XmlElement newElem = doc.CreateElement("palaute");
+
+        string dateNow = Convert.ToString(DateTime.Today.ToShortDateString());
+        newElem.InnerXml = "<pvm>" + dateNow +"</pvm>" +
+            "<tekija>" + this.txtNimi.Text + "</tekija>" +
+            "<opittu>" + this.txtOppinut.Text + "</opittu>" +
+            "<haluanoppia>" + this.txtHaluanoppia.Text + "</haluanoppia>" +
+            "<hyvaa>" + this.txtHyvaa.Text + "</hyvaa>" +
+            "<parannettavaa>" + this.txtParannettavaa.Text + "</parannettavaa>" +
+            "<muuta>" + this.txtMuuta.Text + "</muuta>";
+
+        doc.DocumentElement.SelectNodes("/palautteet")[0].AppendChild(newElem);
+
+        XmlTextWriter wrtr = new XmlTextWriter(MapPath("~/App_Data/Palautteet.xml"), System.Text.Encoding.UTF8);
+        doc.WriteTo(wrtr);
+        wrtr.Close();
+
+        this.txtHaluanoppia.Text = "";
+        this.txtHyvaa.Text = "";
+        this.txtMuuta.Text = "";
+        this.txtNimi.Text = "";
+        this.txtOppinut.Text = "";
+        this.txtParannettavaa.Text = "";
+    }
+
+    protected void nayta_Click(object sender, EventArgs e)
+    {
+        XmlReader reader = XmlReader.Create(MapPath("~/App_Data/Palautteet.xml"));
+        reader.MoveToContent();
+
+        this.teksti.InnerHtml = "<table border=1>";
+        this.teksti.InnerHtml += "<tr><td>Pvm</td>";
+        while(reader.Read())
+        {
+            if(reader.NodeType == XmlNodeType.Element)
+            {
+                switch(reader.Name) {
+                    case "palaute": this.teksti.InnerHtml += "</tr><tr>"; break;
+                    default: this.teksti.InnerHtml += "<td>" + reader.ReadInnerXml() + "</td>"; break;
+                }
+            }
+        }
+        this.teksti.InnerHtml += "</table>";
+    }
+}
